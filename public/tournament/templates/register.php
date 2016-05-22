851ed6ce45025abaa3238ad1f8e9899394e0c441
Denilson <denilsonjvv@gmail.com>
2016-05-22 19:34:04 -0400

    Updated form tags
    Changed form tags and included Labels instead of Placeholders.

--- public/tournament/templates/register.php
+++ public/tournament/templates/register.php
@@ -12,14 +12,16 @@ include_once '../../includes/register.inc.php';
 
     <h1>Simple Registering Form</h1>
 
-    <form action="" method="POST" name="Registration_form">
-
-        <input type="text" name="username" id="username" placeholder="Username" value="" />
-        <input type="email" name="email" id="email" placeholder="Email" value="" />
-        <input type="password" name="password" id="password" placeholder="Password" value="" />
-        <input type="password" name="confirmpwd" id="confirmpwd" placeholder="Password Again" value="" />
-        <input type="button" value="Register" onclick="return regformhash(this.form,this.form.username,this.form.email, this.form.password,this.form.confirmpwd);">
-
+    <form action="" method="POST" class="reg_form" name="Registration_form">
+      <label for="username">Username</label>
+      <input type="text" name="username" id="username" value="">
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email" value="">
+      <label for="password">Password</label>
+      <input type="password" name="password" id="password" value="">
+      <label for="">Repeat Password</label>
+      <input type="password" name="password" id="passwordconfirm"  value="">
+      <input type="button" value="Register" onclick="return regformhash(this.form,this.form.username,this.form.email, this.form.password,this.form.passwordconfirm);" <="" form="">
     </form>
 </div>
 
