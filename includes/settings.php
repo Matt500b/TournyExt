1167630ab6abbaf71b1e34a37e62ee8d6fd4d737
Matt500b <mattbarber1994@aol.com>
2016-05-23 15:20:58 +0100

    Update settings.php

--- includes/settings.php
+++ includes/settings.php
@@ -1,7 +1,10 @@
 <?php 
-define("DEBUG", true);			// Toggle PHP error reporting on/off
+define("DEBUG", true);					// Toggle PHP error reporting on/off
+define("SECURE", FALSE);                // FALSE for development TRUE for production!
 
-define("HOST", "");  			// The host you want to connect to.
-define("USER", "");    			// The database username. 
-define("PASSWORD", "");    		// The database password. 
-define("DATABASE", "");    		// The database name.
+define("HOST", "mysql.csgoberry.com");  // The host you want to connect to.
+define("USER", "matt500b");    			// The database username. 
+define("PASSWORD", "alfoi398bm");    	// The database password. 
+define("DATABASE", "tournext");    		// The database name.
+
+define("TIMEZONE", "Europe/London");	// Your timezone
