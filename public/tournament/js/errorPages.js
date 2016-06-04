a1a46312b5d3bff72d56eb94e9720a4297df236f
Matt500b <mattbarber1994@aol.com>
2016-06-04 01:46:22 +0100

    JS Edits

---
+++ public/tournament/js/errorPages.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $('.open_sgt').click(function() {
+        $('.open-suggestions').slideToggle();
+    });
+
+    function goBack() {
+        window.history.back();
+    };
+
+})
